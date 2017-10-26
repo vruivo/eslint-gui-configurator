61e5be736c73cc900bd49624df6ee948e1bbfed7
Vitor Ruivo <vruivo.code@gmail.com>
2017-10-26 14:59:39 +0100

    beginning of html page creation

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-const rules_reader = require('./source-reader/rules-reader');
+'use strict';
 
-rules_reader();
+const readRules = require('./source-reader/rules-reader');
+
+var rules = readRules();
+
+const html_beginning =
+'<!DOCTYPE html>' +
+'<html lang="en">' +
+'<head>' +
+'  <meta charset="utf-8">' +
+'</head>' +
+'<body>';
+const html_end = '</body></html>';
+
+var html = html_beginning;
+
+rules.forEach(function createHTML(rule) {
+  html += '<a href="'+rule.url +'">'+ rule.name + ' -- '+ rule.description+ '</a><br>';
+});
+
+html += html_end;
+
+const fs = require('fs');
+fs.writeFileSync('index.html', html);
