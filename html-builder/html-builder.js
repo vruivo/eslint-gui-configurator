a6828d6d7bd550a1b5446eba0ddf067891b2b744
Vitor Ruivo <vruivo.code@gmail.com>
2017-11-14 23:56:27 +0000

    organize rules by category; fixable and recommended columns

--- html-builder/html-builder.js
+++ html-builder/html-builder.js
@@ -19,13 +19,31 @@ module.exports = function htmlBuilder(rules, output_file) {
     var html = '<form autocomplete="off"><table style="margin: 0">\n<tbody>\n';
 
     const createRuleLine = html_utils.loadTemplate(__dirname + '/rule_line.htt');
+    const createRuleCategoryLine = html_utils.loadTemplate(__dirname + '/rule_category.htt');
 
+    var rules_organized = {};
+    let category;
+    // organize rules by category
     rules.forEach(function createHTML(rule) {
-      html += createRuleLine({
-        url: rule.url,
-        name: rule.name,
-        description: rule.description,
-        controls: generateRuleControls(rule.schema)
+      category = rule.category;
+      if (!rules_organized[category])
+        rules_organized[category] = [];
+      rules_organized[category].push(rule);
+    });
+
+    // category = null;
+    Object.keys(rules_organized).sort().forEach(function loopItems(category) {
+      // html += '<tr style="background-color:blue"><td colspan="5">'+key+'</td></tr>';
+      html += createRuleCategoryLine({category: category});
+      rules_organized[category].forEach(function createHTML(rule) {
+        html += createRuleLine({
+          recommended: rule.recommended,
+          fixable: rule.fixable,
+          url: rule.url,
+          name: rule.name,
+          description: rule.description,
+          controls: generateRuleControls(rule.schema)
+        });
       });
     });
 
--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var rules = readRules();
 buildHTML(rules, 'output/index.html');
 
 //TODO: environments
-//TODO: fixable, recommended, categories
+//TODO: icons for fixable, recommended
 //TODO: create text rules
 //TODO: generate output, and a big text box for preview
 //TODO: load config from file
